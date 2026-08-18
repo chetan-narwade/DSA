92befa87f5c5f26391c8ea53fbf83ab2302781b3
chetan-narwade <kartiknarawade91@gmail.com>
2026-08-18 13:53:50 +0530

    Time: 5 ms (43.16%) | Memory: 46.5 MB (24.21%) - LeetSync

---
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int largestInteger(int[] nums, int k) {
+        int n = nums.length;
+
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        for (int i = 0; i <= n - k; i++) {
+
+            HashSet<Integer> set = new HashSet<>();
+
+            for (int j = i; j < i + k; j++) {
+                set.add(nums[j]);
+            }
+
+            for (int x : set) {
+                freq.put(x, freq.getOrDefault(x, 0) + 1);
+            }
+        }
+
+        int ans = -1;
+
+        for (int x : freq.keySet()) {
+            if (freq.get(x) == 1) {
+                ans = Math.max(ans, x);
+            }
+        }
+
+        return ans;
+    }
+}
