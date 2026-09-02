99a62eeaaa7fbaf872544241705a8080c138993b
chetan-narwade <kartiknarawade91@gmail.com>
2026-09-02 13:20:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (29.33%) - LeetSync

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return head;
+        } 
+
+        ListNode second = head.next;
+
+        head.next = swapPairs(second.next);
+
+        second.next = head;
+
+        return second;
+    }
+}
