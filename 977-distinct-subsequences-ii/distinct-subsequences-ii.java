5f1bb2250cf391dcbe7adbd06890026b504b9eb3
chetan-narwade <kartiknarawade91@gmail.com>
2026-09-07 11:41:00 +0530

    Time: 4 ms (70.57%) | Memory: 43.7 MB (31.56%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.java
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.java
@@ -7,24 +7,24 @@ class Solution {
 
         long[] dp = new long[n + 1];
 
-        dp[0] = 1;
+        dp[n] = 1;
 
         int[] last = new int[26];
         Arrays.fill(last, -1);
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = n-1; i >= 0; i--) {
 
-            int ch = s.charAt(i - 1) - 'a';
+            int ch = s.charAt(i) - 'a';
 
-            dp[i] = (2 * dp[i - 1]) % MOD;
+            dp[i] = (2 * dp[i + 1]) % MOD;
 
             if (last[ch] != -1) {
                 dp[i] = (dp[i] - dp[last[ch]] + MOD) % MOD;
             }
 
-            last[ch] = i - 1;
+            last[ch] = i + 1;
         }
 
-        return (int) ((dp[n] - 1 + MOD) % MOD);
+        return (int) ((dp[0] - 1 + MOD) % MOD);
     }
 }
